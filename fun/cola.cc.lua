cbf61a31664aaa5580f44e89e6e4a1388e2f3f35
Poilerwags <giestheuws@gmail.com>
2020-09-13 12:29:04 +0200

    colaaaaaa

--- fun/cola.cc.lua
+++ fun/cola.cc.lua
@@ -1,38 +1,21 @@
 {{/*
 	This command allows you to send a cola through YAGPDB.
-	You may send it as embed using `-send [channel] <content>` or as raw with `-send-raw [channel] <content>`.
+	You may send it as embed using `-cola [target]`.
 
 	Recommended trigger: Command trigger with trigger '-cola'
 */}}
 
-{{ $type := or (reFind `raw` .Cmd) "" }}
 {{ $channel := .Channel }}
 {{ $msg := "I'll give you a cola." }}
-{{ if .CmdArgs }}
-	{{ $channelID := "" }}
-	{{ with reFindAllSubmatches `<#(\d+)>` (index .CmdArgs 0) }} {{ $channelID = index . 0 1 }} {{ end }}
-	{{ $temp := getChannel (or $channelID (index .CmdArgs 0)) }}
-	{{ if $temp }}
-		{{ $channel = $temp }}
-		{{ $msg = slice .CmdArgs 1 | joinStr " " }}
-	{{ end }}
-{{ end }}
-{{ if $msg }}
-	{{ if eq $type "raw" }}
-		{{ sendMessageNoEscape $channel.ID $msg }}
-	{{ else }}
-		{{ sendMessage $channel.ID (cembed
-			"author" (sdict "name" "Chika Fujiwara" "icon_url" ("https://imgur.com/TQrsoR2" "256"))
-			"description" $msg
-			"color" 16764159
-			"footer" (sdict "text" (printf "Message sent from #%s" .Channel.Name))
-			"timestamp" currentTime
-      "image" (sdict "url" "https://cdn.discordapp.com/attachments/753012187676541078/754117477633490945/Chika_Cola.jpg")
-		) }}
-	{{ end }}
-	{{ if ne $channel.ID .Channel.ID }}
-		Successfully sent message to <#{{ $channel.ID }}>!
-	{{ end }}
+{{ $target := joinStr " " .CmdArgs }}
+
+{{ if $target }}
+	{{ sendMessage $channel.ID (cembed
+		"author" (sdict "name" "Chika Fujiwara" "url" "https://imgur.com/TQrsoR2")
+		"description" (joinStr "" $target ", " $msg)
+		"color" 16764159
+    "image" (sdict "url" "https://cdn.discordapp.com/attachments/753012187676541078/754117477633490945/Chika_Cola.jpg")
+	) }}
 {{ else }}
-	Sorry, you didn't provide anything to say!
+	Sorry, you didn't provide a target!
 {{ end }}
